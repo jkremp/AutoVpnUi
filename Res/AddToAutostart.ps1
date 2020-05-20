d28bca5d1a118d81d7f2fc4b3300830ba44db73d
Johannes Kremp <jkremp@web.de>
2020-05-20 21:42:56 +0200

    [changed] AddToAutostart.ps1: different relative sourc path

--- Res/AddToAutostart.ps1
+++ Res/AddToAutostart.ps1
@@ -1,6 +1,6 @@
 $ApplicationName = 'AutoVpnUi'
 $Cwd = $MyInvocation.MyCommand.Path | Split-Path -Parent
-$SourcePath = $Cwd + '\Dist'
+$SourcePath = $Cwd
 $objShell = New-Object -ComObject ("WScript.Shell")
 $objShortCut = $objShell.CreateShortcut($env:USERPROFILE + "\Start Menu\Programs\Startup" + "\" + $ApplicationName + ".lnk")
 $objShortCut.TargetPath = $SourcePath + "\" + $ApplicationName + ".exe"

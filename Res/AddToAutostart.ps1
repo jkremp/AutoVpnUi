277a19b6f8904551311941a9736921197da365d3
Johannes <jkremp@web.de>
2021-12-11 13:53:00 +0100

    fix: correct path to MS Windows Autostart folder

--- Res/AddToAutostart.ps1
+++ Res/AddToAutostart.ps1
@@ -2,6 +2,6 @@ $ApplicationName = 'AutoVpnUi'
 $Cwd = $MyInvocation.MyCommand.Path | Split-Path -Parent
 $SourcePath = $Cwd
 $objShell = New-Object -ComObject ("WScript.Shell")
-$objShortCut = $objShell.CreateShortcut($env:USERPROFILE + "\Start Menu\Programs\Startup" + "\" + $ApplicationName + ".lnk")
+$objShortCut = $objShell.CreateShortcut($env:USERPROFILE + "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\" + $ApplicationName + ".lnk")
 $objShortCut.TargetPath = $SourcePath + "\" + $ApplicationName + ".exe"
 $objShortCut.Save()
